7231770979dcc6ca51a68278e5cb72478cf42ddb
DrGoopyDoodle <jmcruzconde@gmail.com>
2024-08-24 17:52:44 -0500

    meme.py
    Tarea 3

---
+++ meme.py
@@ -0,0 +1,18 @@
+meme_dict = {
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Una respuesta común a algo gracioso",
+            "ROFL": "una respuesta a una broma",
+            "SHEESH": "ligera desaprobación",
+            "CREEPY": "aterrador, siniestro",
+            "AGGRO": "ponerse agresivo/enojado",
+            "TQM": "Te quiero mucho",
+            "XD": "Emoticono para mostrar risa o diversión"
+            }
+print("Hola, esto es un diccionario para entender las palabras que no entiendes, haslo 3 veces")
+for i in range(3):
+    word = input("Escribe una palabra (¡con mayúsculas!): ")
+    if word in meme_dict.keys():
+        print(meme_dict[word])
+    else:
+        print("Mmmm.., no lo hemos encontrado, REINICIA el programa")
+print("Listo! si quieres ver mas palabras reinicia el programa")
